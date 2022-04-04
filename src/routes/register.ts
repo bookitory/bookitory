c4586131d764489e724a1e8151c5b1f1142a2f5a
aejin24 <aejin24@naver.com>
2022-04-04 14:44:23 +0900

    modify input file name

--- src/routes/register.ts
+++ src/routes/register.ts
@@ -4,7 +4,7 @@ import { mkdirSync, existsSync, writeFile } from 'fs';
 import { join } from 'path';
 import sharp from 'sharp';
 
-const dir = join(__dirname, "..", "public", "images", "uploads");
+const dir = join(__dirname, '..', 'public', 'images', 'uploads');
 
 const registerRouter = Router();
 
@@ -24,26 +24,26 @@ const _storage = multer.diskStorage({
 const upload = multer({ storage: _storage }); // upload 미들웨어
 
 
-registerRouter.get("/", (req: Request, res: Response, next: NextFunction) => {
-    res.render("register");
+registerRouter.get('/', (req: Request, res: Response) => {
+    res.render('register');
 });
 
-registerRouter.post("/",  upload.single('image'), (req: Request, res: Response, next: NextFunction) => {
+registerRouter.post('/',  upload.single('profile'), (req: Request, res: Response, next: NextFunction) => {
     try {
         sharp(req.file?.path) //파일크기를 압축시킨다
             .resize({ width: 600 }) // 비율을 유지하며 가로 크기 줄이기
             .withMetadata() // 이미지의 exif 데이터 유지
             .toBuffer((err, buffer) => {
-                if (err) throw err;
+                if (err) next(err);
 
                 writeFile(req.file?.path!, buffer, (e) => { // 압축된 파일 기존 파일에 덮어씌우기
-                    if (e) throw e;
+                    if (e) next(e);
                 });
             });
 
         res.json({ filename: `${req.file?.filename}`});
     } catch (error) {
-        console.log(error);   
+        next(error);
     }
 });
 
